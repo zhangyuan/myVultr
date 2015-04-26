e0b2c16829fd878da32f56bc82a351765bab6d60
Yuan Cheung <zhangyuan@users.noreply.github.com>
2015-04-26 10:15:58 +0800

    add preferences

---
+++ myVultr/Controllers/PreferencesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  PreferencesTableViewController.h
+//  myVultr
+//
+//  Created by zhangyuan on 4/26/15.
+//  Copyright (c) 2015 nextcloudmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AccountRepository.h"
+
+@interface PreferencesTableViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UITableViewCell *apiKeyTableViewCell;
+@property (strong, nonatomic) AccountRepository* accountRepository;
+@end
